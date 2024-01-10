fda1de4e203da82802b207363751a750263bf488
Yasmin Hilario Honda <yasminhonda@ibm.com>
2024-01-10 18:42:34 -0300

    feat: styling login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then((m) => m.LoginModule)
   },
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, Inject, OnInit } from '@angular/core';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
@@ -6,9 +7,37 @@ import { SpotifyService } from 'src/app/services/spotify.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
 
   constructor(private spotifyService: SpotifyService) { }
+
+  ngOnInit() {
+    this.circle()
+  }
+
+  circle() {
+    let amount = 150;
+    let body = document.querySelector('.container')
+    let i = 0
+    while (i < amount) {
+      let element = document.createElement('span')
+      element.classList.add("circles")
+      let posX = Math.floor(Math.random() * window.innerWidth)
+      let delay = Math.random() * - 20
+      let duration = Math.random() * 10
+
+      let size = Math.random() * 70
+      element.style.width = 10 + size + 'px'
+      element.style.height = 10 + size + 'px'
+      element.style.left = posX + 'px'
+      element.style.animationDelay = delay + 's'
+      element.style.animationDuration = 10 + duration + 's'
+
+      body?.appendChild(element)
+      i++
+    }
+  }
 
   openLoginPage() {
     window.location.href = this.spotifyService.getLoginUrl()
